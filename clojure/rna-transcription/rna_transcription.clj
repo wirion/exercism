96689b1db9a872f3e0a416f37d7235d41ef19efa
François Wirion <wirion@gmail.com>
2015-03-31 20:14:49 +0200

    Get rna tests to pass

--- clojure/rna-transcription/rna_transcription.clj
+++ clojure/rna-transcription/rna_transcription.clj
@@ -1,4 +1,19 @@
 (ns rna-transcription)
 
-(defn to-rna [dna_string]
-  dna_string)
+(def rna-translation-map
+  {\C \G
+   \G \C
+   \A \U
+   \T \A})
+
+(defn valid-dna? [dna-string]
+  (if (re-matches #"[CGAT]*" dna-string)
+    true
+    false))
+
+(defn to-rna [dna-string]
+  (if (valid-dna? dna-string)
+    (doall (apply str (map
+                       (fn [character] (rna-translation-map character))
+                       dna-string)))
+    (throw (AssertionError. "Non-valid DNA string."))))
