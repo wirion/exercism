2b6055cdcf954481aaa54bfcfb957f5316eba88a
François Wirion <wirion@gmail.com>
2015-04-01 18:42:44 +0200

    Make half of word-count tests pass

--- clojure/word-count/word_count.clj
+++ clojure/word-count/word_count.clj
@@ -1,4 +1,13 @@
-(ns word-count)
+(ns word-count
+  (:require [clojure.string :refer [split]]))
 
-(defn word-count [word]
-  0)
+(defn add-in [coll word]
+  (assoc coll word (let [word-count (coll word)]
+                     (if word-count
+                       (inc word-count)
+                       1))))
+
+(defn word-count [sentence]
+  (let [count-map {}
+        tokens (split sentence #"\s")]
+    (reduce add-in count-map tokens)))
