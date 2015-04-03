750bdd4563e28dbff5f434a15813e0964be8ee09
François Wirion <wirion@gmail.com>
2015-04-03 03:52:37 +0200

    Learn about frequencies function and eliminate code

--- clojure/word-count/word_count.clj
+++ clojure/word-count/word_count.clj
@@ -4,13 +4,5 @@
 (defn- lowercase-tokenize [sentence]
   (map lower-case (split sentence #"[^\p{L}\p{Digit}]+")))
 
-(defn- tally-up [count-map word]
-  (assoc count-map word (let [word-count (count-map word)]
-                     (if word-count
-                       (inc word-count)
-                       1))))
-
 (defn word-count [sentence]
-  (let [count-map {}
-        tokens (lowercase-tokenize sentence)]
-    (reduce tally-up count-map tokens)))
+  (frequencies (lowercase-tokenize sentence)))
