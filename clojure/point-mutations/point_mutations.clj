51193f97458c6a9241af5c945b9aeed60824d16f
François Wirion <wirion@gmail.com>
2015-05-15 14:11:15 +0200

    Make point mutations tests pass

---
+++ clojure/point-mutations/point_mutations.clj
@@ -0,0 +1,5 @@
+(ns point-mutations)
+
+(defn hamming-distance [first-strand second-strand]
+  (when (= (count first-strand) (count second-strand))
+    (count (filter #(= false %) (map = first-strand second-strand)))))
