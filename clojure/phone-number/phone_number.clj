4c0bc5e20ac97ba54fc940e5af96536b6308686f
François Wirion <wirion@gmail.com>
2015-05-16 16:36:35 +0200

    Make all phone number tests pass

--- clojure/phone-number/phone_number.clj
+++ clojure/phone-number/phone_number.clj
@@ -1,7 +1,26 @@
 (ns phone-number)
 
-(defn number [phone-number])
+(def ^:private invalid-number "0000000000")
 
-(defn area-code [phone-number])
+(defn number [phone-number]
+  (let [filtered-number (apply str (filter #(Character/isDigit %) phone-number))
+        filtered-length (count filtered-number)]
+    (if (and (= filtered-length 11)
+             (= (Character/getNumericValue (get filtered-number 0)) 1))
+      (subs filtered-number 1 11)
+      (if (= filtered-length 10)
+        filtered-number
+        invalid-number))))
 
-(defn pretty-print [phone-number])
+(defn area-code [phone-number]
+  (let [normalized-number (number phone-number)]
+    (subs normalized-number 0 3)))
+
+(defn pretty-print [phone-number]
+  (let [normalized-number (number phone-number)]
+    (str "("
+         (subs normalized-number 0 3)
+         ") "
+         (subs normalized-number 3 6)
+         "-"
+         (subs normalized-number 6 10))))
