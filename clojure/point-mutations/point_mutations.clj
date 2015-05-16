f0535b4d47b28990b877695da165aefec381b14a
François Wirion <wirion@gmail.com>
2015-05-16 17:02:10 +0200

    Make hamming distance code clearer

--- clojure/point-mutations/point_mutations.clj
+++ clojure/point-mutations/point_mutations.clj
@@ -2,4 +2,4 @@
 
 (defn hamming-distance [first-strand second-strand]
   (when (= (count first-strand) (count second-strand))
-    (count (filter #(= false %) (map = first-strand second-strand)))))
+    (count (filter false? (map = first-strand second-strand)))))
