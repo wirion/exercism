a705fc5a596e4ae68fc71c57001484f6ccc8025c
François Wirion <wirion@gmail.com>
2015-05-17 17:08:07 +0200

    Make all grade school tests pass

--- clojure/grade-school/grade_school.clj
+++ clojure/grade-school/grade_school.clj
@@ -1,7 +1,10 @@
 (ns grade-school)
 
-(defn add [db name grade-number])
+(defn add [db name grade-number]
+  (merge-with into db {grade-number [name]}))
 
-(defn grade [db grade-number])
+(defn grade [db grade-number]
+  (get db grade-number []))
 
-(defn sorted [db])
+(defn sorted [db]
+  (into (sorted-map) (for [[grade students] db] [grade (sort students)])))
