4bc57d9474ccf49f0488218897d8753230316f67
François Wirion <wirion@gmail.com>
2015-07-15 12:16:14 +0200

    Make nth day pass, rename variable

--- clojure/meetup/meetup.clj
+++ clojure/meetup/meetup.clj
@@ -2,7 +2,7 @@
   (:require [clj-time.core :as t]
             [clj-time.predicates :as pr]))
 
-(def ^:private day-name-to-number-map
+(def ^:private day-keyword-to-number-map
   {:monday 1
    :tuesday 2
    :wednesday 3
@@ -11,20 +11,22 @@
    :saturday 6
    :sunday 7})
 
-(defn- nth-weekday [month year ordinal day-of-week]
-  (let [offset (t/days (t/day-of-week (t/date-time year month 1)))]
-    (t/day
-     (t/minus
-      (t/date-time year month (+ (* ordinal 7) day-of-week)) offset))))
+(defn- nth-weekday [month year which-week day-of-week]
+  (let [first-day-of-month-day-of-week (t/day-of-week (t/date-time year month 1))
+        day-of-month (+ 1
+                        (* (- which-week 1) 7)
+                        (mod (- day-of-week first-day-of-month-day-of-week) 7))]
+    day-of-month))
 
 (defn- last-weekday [month year day-of-week])
 
+;; teenth days are from 13th to 19th of each month
 (defn- teenth-weekday [month year day-of-week])
 
-(defn meetup [month year day-name ordinal-name]
-  (let [day-of-week (day-name-to-number-map day-name)
+(defn meetup [month year day-keyword nth]
+  (let [day-of-week (day-keyword-to-number-map day-keyword)
         day-of-month
-        (case ordinal-name
+        (case nth
           :first (nth-weekday month year 1 day-of-week)
           :second (nth-weekday month year 2 day-of-week)
           :third (nth-weekday month year 3 day-of-week)
