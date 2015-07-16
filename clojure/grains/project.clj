0814cfd4f0cc45f614458ff470a9fc8a8ac99ace
François Wirion <wirion@gmail.com>
2015-07-16 04:02:15 +0200

    Make all tests pass

--- clojure/grains/project.clj
+++ clojure/grains/project.clj
@@ -3,4 +3,5 @@
   :url "https://github.com/exercism/xclojure/tree/master/grains"
   :source-paths [""]
   :test-paths [""]
-  :dependencies [[org.clojure/clojure "1.6.0"]])
+  :dependencies [[org.clojure/clojure "1.6.0"]
+                 [org.clojure/math.numeric-tower "0.0.4"]])
