e6062ec51ba140de7c7827d6faa6febd59947e20
François Wirion <wirion@gmail.com>
2015-09-01 14:31:10 +0200

    Incorporate yurrriq's suggestions

--- clojure/phone-number/phone_number.clj
+++ clojure/phone-number/phone_number.clj
@@ -5,22 +5,18 @@
 (defn number [phone-number]
   (let [filtered-number (apply str (filter #(Character/isDigit %) phone-number))
         filtered-length (count filtered-number)]
-    (if (and (= filtered-length 11)
-             (= (Character/getNumericValue (get filtered-number 0)) 1))
-      (subs filtered-number 1 11)
-      (if (= filtered-length 10)
-        filtered-number
-        invalid-number))))
+    (cond (and (= filtered-length 11)
+               (= (first filtered-number) \1)) (subs filtered-number 1)
+          (= filtered-length 10) filtered-number
+          :else invalid-number)))
 
 (defn area-code [phone-number]
-  (let [normalized-number (number phone-number)]
-    (subs normalized-number 0 3)))
+  (-> phone-number
+      number
+      (subs 0 3)))
 
 (defn pretty-print [phone-number]
   (let [normalized-number (number phone-number)]
-    (str "("
-         (subs normalized-number 0 3)
-         ") "
-         (subs normalized-number 3 6)
-         "-"
-         (subs normalized-number 6 10))))
+    (apply format "(%s) %s-%s"
+           (map #(apply subs normalized-number %)
+                [[0 3] [3 6] [6 10]]))))
