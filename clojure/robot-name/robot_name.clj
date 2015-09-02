783bc394ce069c6a2c0a019a4e34ef4453d3bca2
François Wirion <wirion@gmail.com>
2015-09-02 07:03:30 +0200

    Preliminary, non-working implementation, amend later

--- clojure/robot-name/robot_name.clj
+++ clojure/robot-name/robot_name.clj
@@ -1,11 +1,27 @@
 (ns robot-name)
 
-(defn- random-initials []
-  (str (char (+ 65 (rand-int 25))) (char (+ 65 (rand-int 25)))))
+(def ^:private robot-park (atom []))
 
+;; maybe use a random stream implementation?
+(defn- random-letter []
+  (char (+ 65 (rand-int 25))))
+
+(defn- make-name []
+  (str (random-letter)
+       (random-letter)
+       (format "%03d" (rand-int 1000))))
+
+;; manufacture robot
 (defn robot []
-  (str (random-initials) (format "%03d" (rand-int 1000))))
+  (let [new-robot {:name (make-name)}]
+    (swap! robot-park conj new-robot)
+    new-robot))
 
-(defn robot-name [robot])
+(defn robot-name [robot] (:name robot))
 
-(defn reset-name [robot])
+(defn- find-index [robot-park robot]
+  (.indexOf robot-park robot))
+
+(defn reset-name [robot]
+  (let [robot-index (find-index @robot-park robot)]
+    (swap! robot-park update-in [robot-index :name] #(do % (make-name)))))
