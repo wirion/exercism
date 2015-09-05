499af529f4044fb990e0314f8c8abc8851a627b0
François Wirion <wirion@gmail.com>
2015-09-05 09:14:39 +0200

    Make all gigasecond tests pass

--- clojure/gigasecond/gigasecond.clj
+++ clojure/gigasecond/gigasecond.clj
@@ -1,3 +1,11 @@
-(ns gigasecond)
+(ns gigasecond
+  (:require [clj-time.core :as t]
+            [clj-time.predicates :as pr]))
 
-(defn from [year month day])
+(def ^:private gigasecond 1000000000)
+
+(defn from [year month day]
+  (let [t-date (t/plus
+                (t/date-time year month day)
+                (t/seconds gigasecond))]
+    ((juxt t/year t/month t/day) t-date)))
