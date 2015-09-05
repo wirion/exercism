15096f2ff5773c7e85d329c1886090f2dcc77333
François Wirion <wirion@gmail.com>
2015-09-05 08:26:00 +0200

    Make all robot-name tests pass

--- clojure/robot-name/robot_name.clj
+++ clojure/robot-name/robot_name.clj
@@ -1,7 +1,5 @@
 (ns robot-name)
 
-(def ^:private robot-park (atom []))
-
 ;; maybe use a random stream implementation?
 (defn- random-letter []
   (char (+ 65 (rand-int 25))))
@@ -13,15 +11,9 @@
 
 ;; manufacture robot
 (defn robot []
-  (let [new-robot {:name (make-name)}]
-    (swap! robot-park conj new-robot)
-    new-robot))
+  (atom {:name (make-name)}))
 
-(defn robot-name [robot] (:name robot))
-
-(defn- find-index [robot-park robot]
-  (.indexOf robot-park robot))
+(defn robot-name [robot] (:name @robot))
 
 (defn reset-name [robot]
-  (let [robot-index (find-index @robot-park robot)]
-    (swap! robot-park update-in [robot-index :name] #(do % (make-name)))))
+  (swap! robot assoc :name (make-name)))
