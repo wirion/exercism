5bb30ddfa19e543b5ea451bfc9e95b77061ef33c
François Wirion <wirion@gmail.com>
2015-09-06 01:42:22 +0200

    Preliminary code, not all tests pass

--- clojure/scrabble-score/scrabble_score.clj
+++ clojure/scrabble-score/scrabble_score.clj
@@ -1,5 +1,30 @@
-(ns scrabble_score)
+(ns scrabble-score
+  (:require [clojure.string :refer [upper-case]]))
 
-(defn score-letter [letter])
+(def ^:private score-classes
+  [["AEIOULNRST" 1]
+   ["DG" 2]
+   ["BCMP" 3]
+   ["FHVWY" 4]
+   ["K" 5]
+   ["JX" 8]
+   ["QZ" 10]])
 
-(defn score-word [word])
+(def ^:private letter->score
+  (apply merge (map (fn [class]
+                      (apply #(zipmap %1 (repeat %2)) class))
+                    score-classes)))
+
+;; (def ^:private score (merge (zipmap "AEIOULNRST" (repeat 1))
+;;                             (zipmap "DG" (repeat 2))
+;;                             (zipmap "BCMP" (repeat 3))
+;;                             (zipmap "FHVWY" (repeat 4))
+;;                             (zipmap "K" (repeat 5))
+;;                             (zipmap "JX" (repeat 8))
+;;                             (zipmap "QZ" (repeat 10))))
+
+(defn score-letter [letter]
+  (letter->score (Character/toUpperCase letter)))
+
+(defn score-word [word]
+  (apply + (map score-letter word)))
