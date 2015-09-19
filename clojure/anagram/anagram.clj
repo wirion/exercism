f65b2ab1d0123fed0da653388fdef9064489a510
François Wirion <wirion@gmail.com>
2015-09-19 13:41:38 +0200

    Fix order of doc-string argument in anagram.clj

--- clojure/anagram/anagram.clj
+++ clojure/anagram/anagram.clj
@@ -2,8 +2,8 @@
   (:require [clojure.string :refer [lower-case]]))
 
 (defn anagrams-for
-  [word candidates]
   "Filters the list for candidate anagrams of word"
+  [word candidates]
   (let [letter-count (frequencies (lower-case word))]
     (remove #(= (lower-case %) (lower-case word))
             (filter #(= letter-count (frequencies (lower-case %))) candidates))))
