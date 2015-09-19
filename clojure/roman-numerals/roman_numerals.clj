196085c67e63fd370ed1d96d946450e37c77c3e2
François Wirion <wirion@gmail.com>
2015-09-19 14:14:26 +0200

    Fix confusion over conversion direction
    We're converting from Arabic numerals to Roman numerals, not vice
    versa.

--- clojure/roman-numerals/roman_numerals.clj
+++ clojure/roman-numerals/roman_numerals.clj
@@ -4,12 +4,7 @@
 
 (defn numerals
   "Convert Arabic numbers to Roman numbers"
-  [roman-number]
-  (reverse (seq roman-number)))
+  [arabic-number]
+  arabic-numberc)
 
-;; Hmm, how to parse this?
-;; I'm thinking state machine, maybe stateful transducer.
-;; We also need to establish an order: I < V < X < L < C < D < M
-;; The letters following V, L, D are always smaller than themselves, and V, L, D may never appear next to themselves.
-;; X, C, M can appear as singles, doubles or triples.
 ;; Treat arabic number as powers of ten from right to left (i.e. digit by digit)?
